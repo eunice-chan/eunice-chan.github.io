4853f16e3905f5bfe48bf3c4cd0a4552c71730bb
Eunice Chan <ekchan@berkeley.edu>
2019-05-21 00:41:37 -0700

    Mostly done with exp:

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-var m = new Date().getMonth();
-var s = "url('Public Domain Images/";
-if (m < 2 || m == 11) {
-  s += "winter";
-} else if (m < 5) {
-  s += "spring";
-} else if (m < 8) {
-  s += "summer";
-} else {
-  s += "fall";
-}
-s += ".jpg')";
-document.getElementById("intro").style.backgroundImage = s;
+$('.component').click(function(){
+    $(this).children('div').fadeIn(1000);
+})
+$('button').click(function(){
+    $(this).closest('.overlay').fadeOut(1000);
+})
