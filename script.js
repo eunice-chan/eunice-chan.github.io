6f9a6eb981abac990f74f39595de6b6f4923b380
Eunice Chan <ekchan@berkeley.edu>
2019-05-22 23:03:37 -0700

    Finished each section.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function delay (URL) {
+    setTimeout( function() { window.location = URL }, 500);
+};
+
 $('.component').click(function(){
     $(this).children('div').fadeToggle(500);
-})
+});
+
+$('.menu').click(function(){
+  if (!$(this).attr('id')) {
+    $('#exit').addClass('loadout').addClass('fullscreen');
+  }
+});
